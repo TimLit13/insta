d9910835254b0c213fcf19ca1f23e17f5f362a0c
TimLit13 <litidm@gmail.com>
2021-07-14 11:14:56 +0300

    Add user avatar stub with gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
       notice: "alert-info"
     }[name.to_sym] || name
   end
+
+  def avatar_url user
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
+  end
 end
