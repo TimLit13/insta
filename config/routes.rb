5a9ff0633039b2b7a927b574159d632cb2c38cde
TimLit13 <litidm@gmail.com>
2021-07-19 11:46:45 +0300

    Add routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   resources :users, only: [:show]
+  resources :posts, only: [:index, :show, :create] do
+    resources :photos, only: [:create]
+  end
 
 end
