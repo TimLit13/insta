b32ac5b0cc8c86c1827587d184e1c3e8b0c431f1
TimLit13 <litidm@gmail.com>
2021-07-19 11:09:38 +0300

    Add settings for cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,5 @@
+Cloudinary.config do |config|
+  config.cloud_name = "tim13"
+  config.api_key = "464822575294159"
+  config.api_secret = "VmRc8CGWsAPfX40nEJh9A4CiUNk"
+  config.cdn_subdomain = true
