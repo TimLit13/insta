070371ecbf4f96d6479c68e86af7a86312a96452
TimLit13 <litidm@gmail.com>
2021-07-22 11:09:13 +0300

    Add routes for likes with posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :photos, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
 
 end
